7eeb8a4f7fcca49a976743949564dcb21ca36345
Zurc <cruzjurado@gmail.com>
2017-11-24 23:29:01 +0000

    on DELETE show deleted item

--- server.js
+++ server.js
@@ -52,8 +52,7 @@ app.delete('/todos/:id', function(req,res) {
     res.status(404).json({error: "no todo found with that id"});
   } else {
     todos = _.without(todos, matchedTodo);
-    // todos.slice(todos.indexOf(matchedTodo), 1);
-    res.json(todos);
+    res.json(matchedTodo);
   }
 
 }),
