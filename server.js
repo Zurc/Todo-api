d57594858509c6e235190b0809d7aad3f83a622e
Zurc <cruzjurado@gmail.com>
2017-11-27 16:31:09 +0000

    Add todo model and POST /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var db = require('./db.js');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -50,60 +51,70 @@ app.get('/todos/:id', function(req, res) {
 app.post('/todos', function(req, res) {
     var body = _.pick(req.body, 'completed', 'description');
 
-    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send();
-    }
+    // if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //     return res.status(400).send();
+    // }
 
-    body.description = body.description.trim();
+    // body.description = body.description.trim();
 
-    body.id = todoNextId++;
+    // body.id = todoNextId++;
 
-    todos.push(body);
+    // todos.push(body);
 
-    res.json(body);
+    // res.json(body);
+
+    db.todo.create(body).then(function(todo) {
+            res.json(todo.toJSON());
+        },
+        function(e) {
+            res.status(400).send('todo not found');
+        })
 })
 
 // DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
-        var todoId = +(req.params.id);
-        var matchedTodo = _.findWhere(todos, { id: todoId });
+    var todoId = +(req.params.id);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
 
-        if (!matchedTodo) {
-            res.status(404).json({ error: "no todo found with that id" });
-        } else {
-            todos = _.without(todos, matchedTodo);
-            res.json(matchedTodo);
-        }
-
-    }),
-
-    // PUT /todos/:id
-    app.put('/todos/:id', function(req, res) {
-        var todoId = +(req.params.id);
-        var matchedTodo = _.findWhere(todos, { id: todoId });
-        var body = _.pick(req.body, 'completed', 'description');
-        var validAtributes = {};
-
-        if (!matchedTodo) {
-            return res.status(404).send();
-        }
-
-        if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-            validAtributes.completed = body.completed;
-        } else if (body.hasOwnProperty('completed')) {
-            return res.status(400).send();
-        }
-
-        if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-            validAtributes.description = body.description;
-        } else if (body.hasOwnProperty('description')) {
-            return res.status(400).send();
-        }
-
-        _.extend(matchedTodo, validAtributes);
+    if (!matchedTodo) {
+        res.status(404).json({ error: "no todo found with that id" });
+    } else {
+        todos = _.without(todos, matchedTodo);
         res.json(matchedTodo);
-    });
+    }
 
-app.listen(PORT, function() {
-    console.log('Express listening on port ' + PORT + '!...');
+})
+
+// PUT /todos/:id
+app.put('/todos/:id', function(req, res) {
+    var todoId = +(req.params.id);
+    var matchedTodo = _.findWhere(todos, { id: todoId });
+    var body = _.pick(req.body, 'completed', 'description');
+    var validAtributes = {};
+
+    if (!matchedTodo) {
+        return res.status(404).send();
+    }
+
+    if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+        validAtributes.completed = body.completed;
+    } else if (body.hasOwnProperty('completed')) {
+        return res.status(400).send();
+    }
+
+    if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+        validAtributes.description = body.description;
+    } else if (body.hasOwnProperty('description')) {
+        return res.status(400).send();
+    }
+
+    _.extend(matchedTodo, validAtributes);
+    res.json(matchedTodo);
+});
+
+db.sequelize.sync().then(function() {
+
+    app.listen(PORT, function() {
+        console.log('Express listening on port ' + PORT + '!...');
+    });
 });
