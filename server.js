7f16d83d3adfc8d9f97b4f2179eac118e240b16d
Zurc <cruzjurado@gmail.com>
2017-11-28 11:24:35 +0000

    Add POST users/login and authenticate class method

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
+var bcrypt = require('bcrypt');
 var db = require('./db.js');
 
 var app = express();
@@ -122,6 +123,16 @@ app.post('/users', function(req, res) {
         res.json(user.toPublicJSON());
     }, function(e) {
         res.status(400).json(e);
+    });
+});
+
+app.post('/users/login', function(req, res) {
+    var body = _.pick(req.body, 'email', 'password');
+
+    db.user.authenticate(body).then(function(user) {
+        res.json(user.toPublicJSON());
+    }, function() {
+        res.status(401).send();
     });
 });
 
